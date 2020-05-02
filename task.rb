d5af1dae61ece917d919b8a8e42a4c6714aa9da5
kazutaka <k89ut3903@gmail.com>
2020-05-02 23:18:04 +0900

    Ruby基礎q9-11提出

--- task.rb
+++ task.rb
@@ -76,21 +76,37 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
+  names.each.with_index(1) do |name, index|
+    puts "会員No.#{index} #{name}さん"
+  end
 end
 
 def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
-
+  foods.each do |food|
+    puts food.include?("うに") ? "好物です" : "まぁまぁ好きです"
+  end
 end
 
 def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
-
+  puts "ユーザーの趣味一覧"
+  sports.each.with_index(1) do |sport, index|
+    if sport.instance_of?(Array)
+      if sport.count >= 3
+        puts "No#{index} #{sport[0]}"
+        puts "No#{index+1} #{sport.last}"
+      else sport.count
+        puts "No#{index} #{sport[0]}"
+      end
+    else
+      puts "No#{index} #{sport}"
+    end
+  end
 end
 
 def q12
