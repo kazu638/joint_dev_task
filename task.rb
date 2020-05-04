e21c8865b7eb3f060e903e8ebe9faf6f575d3443
kazu638 <k89ut3903@gmail.com>
2020-05-04 23:10:21 +0900

    Ruby基礎q16修正

--- task.rb
+++ task.rb
@@ -137,7 +137,7 @@ def q16
   ]
 
   # 以下に回答を記載
-  puts users.map{|user| "私の名前は#{user[:name]}です。年齢は#{user[:age]}歳です。"}
+  users.each { |user| puts "私の名前は#{user[:name]}です。年齢は#{user[:age]}歳です。" }
 end
 
 class UserQ17
